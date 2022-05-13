17590d218faa8a77845ef925c8ee7ec1fd6067af
Aleksandra Markovic <aleksandra.markovickg@gmail.com>
2022-05-13 19:59:17 +0200

    Minor typo fix

--- osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
@@ -2,6 +2,7 @@ package p13_05_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello borld!");
+        System.out.println("Hello World!");
+        System.out.println("Novi print");
     }
 }
