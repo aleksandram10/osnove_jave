174f39ccf6f220a39663e0fb44f08c2b260f4367
Aleksandra Markovic <aleksandra.markovickg@gmail.com>
2022-05-13 20:36:58 +0200

    Git Zadatak1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,13 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git status");
+        System.out.println("git add");
+        System.out.println("git commit");
+        System.out.println("git push");
+        System.out.println("git restore");
+
+
+    }
+}
